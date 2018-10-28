5c9a2cd12c9c6b6b91901f0c9d08a3a44e77de33
malyda <malyce.dvd@gmail.com>
2018-10-28 15:10:45 +0100

    Added image for StackLayout and Grid

---
+++ Gopas.XamIntro/Gopas.XamIntro/Course/1GUIComponents/RelativeAndAbsoluteLayout.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Gopas.XamIntro.Course._1GUIComponents
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RelativeAndAbsoluteLayout : ContentPage
+	{
+		public RelativeAndAbsoluteLayout ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
