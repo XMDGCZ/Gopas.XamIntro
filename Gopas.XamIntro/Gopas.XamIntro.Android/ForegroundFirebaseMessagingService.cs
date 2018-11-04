e28e10f5988cb3eea3fd88aa3d53249fc6576d57
malyda <malyce.dvd@gmail.com>
2018-11-04 10:57:14 +0100

    Working sample for AppCenterPushNotification (Android)

---
+++ Gopas.XamIntro/Gopas.XamIntro.Android/ForegroundFirebaseMessagingService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Firebase.Messaging;
+
+namespace Gopas.XamIntro.Droid
+{
+    /// <summary>
+    /// Bug workaround for event PushNotificationReceived not called for application running in foreground
+    /// </summary>
+    [Service]
+    [IntentFilter(new[] { "com.google.firebase.MESSAGING_EVENT" })]
+    public class ForegroundFirebaseMessagingService : FirebaseMessagingService
+    {
+        const string TAG = "MyFirebaseMsgService";
+        public override void OnMessageReceived(RemoteMessage message)
+        {
+            Log.Debug(TAG, "From: " + message.From);
+            Log.Debug(TAG, "Notification Message Body: " + message.GetNotification().Body);
+            //MessagingCenter.Send<object, string>(this, "Hi", message.GetNotification().Body);
+        }
+    }
+}
---
+++ Gopas.XamIntro/Gopas.XamIntro/Course/5Push/AppCenterPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using Microsoft.AppCenter.Push;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Gopas.XamIntro.Course._5Push
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AppCenterPage : ContentPage
+    {
+        public AppCenterPage()
+        {
+            InitializeComponent();
+
+            AppCenterInit();
+        }
+
+
+        async void AppCenterInit()
+        {
+  
+
+               Debug.WriteLine("Install ID:"+ await AppCenter.GetInstallIdAsync() );
+            
+        }
+    }
+}
