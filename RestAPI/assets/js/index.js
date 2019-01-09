2acbab1a5108b6cd91922e46e1c7ee906a53a2cf
David Malý <malyce.dvd@gmail.com>
2019-01-09 09:49:44 +0100

    Added materialize

--- RestAPI/assets/js/index.js
+++ RestAPI/assets/js/index.js
@@ -29,30 +29,21 @@ function getItems() {
             getCount(data.length);
             $.each(data, function (key, item) {
                 const tr = $("<tr></tr>")
-                    .append(
-                        $("<td></td>").append(
-                            $("<input/>", {
-                                type: "checkbox",
-                                disabled: true
-                            })
-                        )
-                    )
                     .append($("<td></td>").text(item.name))
                     .append(
-                        $("<td></td>").append(
+                    $("<td class='table-button-td'></td>").append(
                             $("<button>Edit</button>").on("click", function () {
                                 editItem(item.id);
                             })
                         )
                     )
-                    .append(
-                        $("<td></td>").append(
-                            $("<button>Delete</button>").on("click", function () {
-                                deleteItem(item.id);
-                            })
-                        )
-                    );
-
+                .append(
+                    $("<td class='table-button-td'></td>").append(
+                        $("<button>Delete</button>").on("click", function () {
+                            deleteItem(item.id);
+                        })
+                    )
+                )
                 tr.appendTo(tBody);
             });
 
