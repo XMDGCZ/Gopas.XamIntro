52569fbb55c60344473cfc43ebf96674664ca377
malyda <malyce.dvd@gmail.com>
2019-03-16 08:59:54 +0100

    Changed MainActivity label

--- Gopas.XamIntro/Gopas.XamIntro.Android/SplashActivity.cs
+++ Gopas.XamIntro/Gopas.XamIntro.Android/SplashActivity.cs
@@ -12,7 +12,7 @@ using Android.Widget;
 
 namespace Gopas.XamIntro.Droid
 {
-    [Activity(Label = "SplashActivity", Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true)]
+    [Activity( Theme = "@style/SplashTheme", MainLauncher = true, NoHistory = true)]
     public class SplashActivity : Activity
     {
         protected override void OnCreate(Bundle savedInstanceState)
